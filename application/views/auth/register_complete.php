5243e1987aeb0be711157d28cfcfb67271161344
Danupong55 <151014296+Danupong55@users.noreply.github.com>
2024-01-22 14:51:43 +0700

    Add regis_complete

---
+++ application/views/auth/register_complete.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+  <div class="container">
+    <div class="row text-center mt-2">
+      <div class="col">
+        <div class="p-5">
+          <img src="../../../assets/img/check.png" style="width: 150px;">
+        </div>
+        <h2>
+          Congratulations! Registration Successfully!
+        </h2>
+        <h2>
+          Please check your email and click link inside your email for activation your new account.
+        </h2>
+        <div class="text-center m-5">
+          <a href="/auth/login" class="btn btn-primary btn-lg"><img src="../../../assets/img/user.png"></img> Sign in</a>
+          <a href="/" class="btn btn-link link-secondary"><img src="../../../assets/img/home.png"></img> Homepage</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
